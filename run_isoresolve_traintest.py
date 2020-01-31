94fdbbb1ae67bd347412a97f580392082b5dd07e
Hongdong Li <48688298+genemine@users.noreply.github.com>
2020-01-31 21:16:33 +0800

    Add files via upload

---
+++ run_isoresolve_traintest.py
@@ -0,0 +1,44 @@
+import glob,sys,os
+from isoresolve_lib import *
+
+
+
+# input data
+inputdir='data/goterm_traintest'
+outdir='output_traintest/'
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+fold=5
+k=20 # dimension of LV space
+lmd=0.01
+
+# MAIN
+term=inputdir.split('/')[-1]
+ret=isoresolve_train_test(inputdir,fold,A=k,lmd=lmd)
+outfile=outdir+'/'+term+'.isoresolve.auc.auprc'
+iso_scorefile=outdir+'/'+term+'.isoresolve.iso.score'
+gene_scorefile=outdir+'/'+term+'.isoresolve.gene.score'
+gene_scorefile=outdir+'/'+term+'.isoresolve.gene.score'
+
+f=open(outfile,'w')
+
+f.write('AUC\t'+str(ret['auc'])+'\n')
+#f.write('AUC_sig\t'+str(ret['auc_sig'])+'\n')
+#f.write('AUC_mig\t'+str(ret['auc_mig'])+'\n')
+f.write('AUPRC\t'+str(ret['auprc'])+'\n')
+#f.write('AUPRC_sig\t'+str(ret['auprc_sig'])+'\n')
+#f.write('AUPRC_mig\t'+str(ret['auprc_mig'])+'\n')
+f.write('Aopt\t'+str(ret['Aopt'])+'\n')
+f.write('AUC_nlv\t'+str(ret['auc_nlv'])+'\n')
+f.close()
+
+print('AUC:'+str(ret['auc']))
+
+isoscore=pd.DataFrame(ret['isoscore'])
+isoscore.to_csv(iso_scorefile,index=None,header=None,sep='\t')
+
+
+genescore=pd.DataFrame(ret['gene_score'])
+genescore.to_csv(gene_scorefile,header=None,sep='\t')
+
+
